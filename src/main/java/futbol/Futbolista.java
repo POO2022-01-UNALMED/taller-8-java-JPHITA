096d193687e436429f1fa952cf4949926dddf6c9
JPHITA <jphita18@gmail.com>
2022-05-20 19:27:26 -0500

    solucion

---
+++ src/main/java/futbol/Futbolista.java
@@ -0,0 +1,57 @@
+package futbol;
+
+public abstract class Futbolista implements Comparable<Object> {
+    private String nombre;
+    private int edad;
+    private final String posicion;
+
+    public Futbolista(String nombre, int edad, String posicion){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.posicion = posicion;
+    }
+
+    public Futbolista(){
+        this("Maradona", 30, "delantero");
+    }
+
+    // public int compareTo(Object f){
+    //     return 0;
+    // }
+
+    public String toString(){
+        return "El futbolista "+ this.nombre
+                + " tiene "+ this.edad
+                + ", y juega de " + this.posicion;
+    }
+
+    public boolean equals(Futbolista f){
+        return super.equals(f);
+    }
+
+    public abstract boolean jugarConLasManos();
+
+    // getters y setteres
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    
+}
---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,29 @@
+package futbol;
+
+public class Portero extends Futbolista{
+    public short golesRecibidos;
+    public byte dorsal;
+
+    public Portero(String nombre, int edad, short goles, byte dorsal){
+        super(nombre, edad, "Portero");
+        this.golesRecibidos = goles;
+        this.dorsal = dorsal;
+    }
+
+    public boolean jugarConLasManos(){
+        return true;
+    }
+
+    public String toString(){
+        return super.toString()
+                + " con el dorsal " + this.dorsal
+                + ". Le han marcado " + this.golesRecibidos;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return Math.abs(this.golesRecibidos - ((Portero) o).golesRecibidos);
+    }
+
+    
+}
